f380c940653388062b9cfb77e6921659fa666013
Deniz Setyawan <denizsetyawan95@gmail.com>
2021-11-23 13:09:29 +0700

    api create done

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaction;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class TransactionController extends Controller
@@ -32,7 +34,29 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => ['required'],
+            'amount' => ['required','numeric'],
+            'type' => ['required','in:expense,revenue']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        try{
+            $transaction = Transaction::create($request->all());
+            $response = [
+                'message' => 'Transaction created',
+                'data' => $transaction
+            ];
+            
+            return response()->json($response, Response::HTTP_CREATED);
+        }catch(QueryException $e) {
+            return response()->json([
+                'message' => "Failed " . $e->errorInfo
+            ]);
+        }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/transaction', [TransactionController::class, 'index']);
+Route::post('/transaction', [TransactionController::class, 'store']);

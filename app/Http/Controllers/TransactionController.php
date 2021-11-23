6a424eb1f4d20edc1684602660c8cd441bda3a4d
Deniz Setyawan <denizsetyawan95@gmail.com>
2021-11-23 13:33:11 +0700

    api getbyid && destroy done

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -68,6 +68,14 @@ class TransactionController extends Controller
     public function show($id)
     {
         //
+        $transaction = Transaction::findOrFail($id);
+
+        $response = [
+            'message' => 'Detail of transaction resource',
+            'data' => $transaction
+        ];
+
+        return response()->json($response, Response::HTTP_OK);
     }
 
     /**
@@ -115,5 +123,19 @@ class TransactionController extends Controller
     public function destroy($id)
     {
         //
+        $transaction = Transaction::findOrFail($id);
+        
+        try{
+            $transaction->delete();
+            $response = [
+                'message' => 'Transaction deleted',
+            ];
+            
+            return response()->json($response, Response::HTTP_OK);
+        }catch(QueryException $e) {
+            return response()->json([
+                'message' => "Failed " . $e->errorInfo
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/transaction', [TransactionController::class, 'index']);
-Route::post('/transaction', [TransactionController::class, 'store']);
-Route::put('/transaction/{id}', [TransactionController::class, 'update']);
+// Route::get('/transaction', [TransactionController::class, 'index']);
+// Route::post('/transaction', [TransactionController::class, 'store']);
+// Route::get('/transaction/{id}', [TransactionController::class, 'show']);
+// Route::put('/transaction/{id}', [TransactionController::class, 'update']);
+// Route::delete('/transaction/{id}', [TransactionController::class, 'destroy']);
+
+Route::resource('/transaction', TransactionController::class)->except('create','edit');

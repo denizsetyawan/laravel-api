8e3d56304beea82f6166fe4dd4a7854895d8b2cf
Deniz Setyawan <denizsetyawan95@gmail.com>
2021-11-24 18:50:31 +0700

    fix validasi

--- akunt-client/src/views/transaction/Create.vue
+++ akunt-client/src/views/transaction/Create.vue
@@ -14,22 +14,22 @@
                             <div class="mb-3">
                                 <label class="form-label">Title</label>
                                 <input type="text" class="form-control" v-model="transaction.title">
-                                <div class="text-danger">
-                                    {{ validation.title }}
+                                <div v-if="validation.title" class="text-danger">
+                                    {{ validation.title[0] }}
                                 </div>
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Amount</label>
                                 <input type="number" class="form-control" v-model="transaction.amount">
-                                <div class="text-danger">
-                                    {{ validation.amount }}
+                                <div v-if="validation.amount" class="text-danger">
+                                    {{ validation.amount[0] }}
                                 </div>
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Time</label>
                                 <input type="text" class="form-control" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.time">
-                                <div class="text-danger">
-                                    {{ validation.time }}
+                                <div v-if="validation.time" class="text-danger">
+                                    {{ validation.time[0] }}
                                 </div>
                             </div>
                             <div class="mb-3">
@@ -38,8 +38,8 @@
                                     <option value="expense">Expense</option>
                                     <option value="revenue">Revenue</option>
                                 </select>
-                                <div class="text-danger">
-                                    {{ validation.type }}
+                                <div v-if="validation.type" class="text-danger">
+                                    {{ validation.type[0] }}
                                 </div>
                             </div>
                             <button class="btn btn-outline-primary">Submit</button>

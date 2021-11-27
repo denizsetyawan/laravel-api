47a2196d70490416a6a59b718118d1601cba4663
Deniz Setyawan <denizsetyawan95@gmail.com>
2021-11-27 11:17:07 +0700

    fix api auth

--- akunt-api/routes/api.php
+++ akunt-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', [AuthController::class, 'signin']);
+Route::post('register', [AuthController::class, 'signup']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
